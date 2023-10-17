f2cff55cdb721f70148d0e18ce7b5d485c8ccfd3
Yevhenii Saimov <63614260+Yevgenesis@users.noreply.github.com>
2023-10-18 01:34:57 +0200

    Update sleeper.sh

--- sleeper.sh
+++ sleeper.sh
@@ -7,14 +7,14 @@
 	#sleep 10
 	done
 
-# Можно было через cat /proc/cpuinfo но так красивее)
-lscpu >> processor_info.txt
+# Можно было через cat /proc/cpuinfo но так вроде тоже можно
+lscpu > file.txt
 
 # Для поиска от начала строки использовал ^ из регулярок
-cat /etc/os-release | grep ^NAME=
+cat /etc/os-release | grep ^NAME= >> file.txt
 
 # Тут тоже использовал ^ но мозг закипел пока разобрался с "-n" и "p" в конце sed
-sed -n 's/^NAME="//p' /etc/os-release | awk '{print$1}'
+sed -n 's/^NAME="//p' /etc/os-release | awk '{print$1}' >> file.txt
 
 	for run in {50..100}
 	do
